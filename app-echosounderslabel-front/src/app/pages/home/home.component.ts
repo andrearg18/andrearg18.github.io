1b98bec1a48e91033cad431828275dfe9d623b22
W2M\andrea.rodriguez <andrea.rodriguez@externos.w2m.com>
2025-11-18 20:43:35 +0100

    Title and form background and installed material

--- app-echosounderslabel-front/src/app/pages/home/home.component.ts
+++ app-echosounderslabel-front/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public emailForm!: UntypedFormGroup
 
+  constructor(
+    private formBuilder: UntypedFormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.initForm()
+  }
+
+  private initForm(): void {
+    this.emailForm = this.formBuilder.group({
+      subject: ['A', Validators.required],
+      email: ['', Validators.required],
+      link: ['', Validators.required],
+      about: ['', Validators.required],
+    })
+
+  }
 }
--- app-echosounderslabel-front/src/app/pages/home/home.module.ts
+++ app-echosounderslabel-front/src/app/pages/home/home.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
-
+const MATERIAL = [
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+]
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
+    MATERIAL,
     CommonModule,
-    HomeRoutingModule
+    ReactiveFormsModule,
+    HomeRoutingModule,
   ],
 })
 export class HomeModule { }
