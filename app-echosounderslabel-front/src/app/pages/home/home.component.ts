7f0e92da086d64a5b00c83a17f8fcea5a5afbd68
W2M\andrea.rodriguez <andrea.rodriguez@externos.w2m.com>
2025-12-01 20:11:52 +0100

    Test fix form 2

--- app-echosounderslabel-front/src/app/pages/home/home.component.ts
+++ app-echosounderslabel-front/src/app/pages/home/home.component.ts
@@ -29,7 +29,7 @@ export class HomeComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    console.info('v4 Test fix form 1')
+    console.info('v5 Test fix form 2')
     this.initForm()
   }
 
